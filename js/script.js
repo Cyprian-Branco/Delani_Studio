91e6fad156bc6a5f47eba28666d8f7effe857159
Cyprian Bowen <branco1lelgo@gmail.com>
2019-08-25 12:18:45 +0300

    add a js function that will link up mailchimp and the website for mail sending

--- js/script.js
+++ js/script.js
@@ -25,27 +25,40 @@ $(function(){
 })
 //read contact us
 $(function(){
+    var name = document.getElementById("name").value
+    var email = document.getElementById("email").value
+    var message = document.getElementById("message").value
     $(".send").click(function(event){
-        event.preventDefault();
-
-        var name = document.getElementById("name").value
-        var email = document.getElementById("email").value
-        var message = document.getElementById("message").value
-    
-        if (name.length > 2){
-            console.log("Name Valid")
+        if (!name || !email || !message){
+            document.getElementById("result").innerText = "Please check your entries"
         }else{
-            alert("Invalid name")
-        }
-        if (email.length > 2 && email.includes("@") && email.includes(".")){
-            console.log("email valid")
-        }else {
-            alert("Invalid email")
-        }if (message.lenght >2){
             document.getElementById("result").innerText = "Thank you for your message"
-        }else {
-            document.getElementById("result").innerText = "No message sent"
         }
     })
 })
 //sending message using mailchimp
+$(function(){
+    $(".form").bind(".send", function(){
+        event.preventDefault()
+        var name1 = $(".name").value
+        var email1 = $(".email").value
+        var message1 = $(".message").value
+        var url = "https://us20.api.mailchimp.com/2.0/lists/subscribe.json?" + "apikey=81ce4a8ff3d33b570692d4621346bef4-us3"
+        "id=665257" + "&email[email]=" + email1 +
+        "&merge_vars[NAME]=" + name1 +
+        "&merge_vars[NAME]=" + message1 +
+        "&send_welcome=false"
+
+        $.ajax({
+            type: "POST",
+            url: url,
+            dataType: "json",
+            success: function(data){
+                $(".form").html("Thank you")
+            },
+            error: function(jqXHR, textStatus, errorThrown){
+                alert(errorThrown)
+            }
+        })
+    })
+})
